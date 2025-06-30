244679aeb39fa58f67138903f678e61ee3ce3808
marianaalmeida123 <mariana.almeida.silva@escola.pr.gov.br>
2025-06-30 10:37:41 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+     
+    'Programação',
+    'Como se diz OI em inglês?',
+    'Oi em inglês é HI'
+) 
+ 
+
+criaCartao(
+     'Programação',
+    'Como se diz OI em espanhol?',
+    'Oi em espanhol é HOLA'
+  
+) 
+
+criaCartao(
+     'Arte',
+    'Quem que pintou a Mona Lisa?',
+    'Leonardo da Vinci'
+) 
+ 
